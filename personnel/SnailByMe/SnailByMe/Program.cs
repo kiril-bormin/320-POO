722c66949257f5b7ea7c5e2b04cb77877a2cafa9
Kiril Bormin <borminnkiril@gmail.com>
2025-08-29 11:01:15 +0200

    Snail fait par moi sans POO

---
+++ personnel/SnailByMe/SnailByMe/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SnailByMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int actualPos = 2;
+            
+            Console.SetCursorPosition(0, 12);
+            Console.CursorVisible = false;
+            do
+            {
+                Console.SetCursorPosition(actualPos, 12);
+                Console.WriteLine("_@_ö");
+                Console.SetCursorPosition(actualPos - 1, 12);
+                Console.WriteLine(" ");
+                Console.SetCursorPosition(actualPos, 12);
+                actualPos ++;
+                Thread.Sleep(150);
+                
+            } while (actualPos < 30);
+
+            Console.WriteLine("____");
+        }
+    }
+}
