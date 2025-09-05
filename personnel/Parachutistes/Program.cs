32fa282d4508e4df7fd618142e74e247d32eb057
Kiril Bormin <borminnkiril@gmail.com>
2025-09-05 09:20:00 +0200

    feat : Ce commit ajoute les classe 'Para' et 'Plane' dans le programme

---
+++ personnel/Parachutistes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Parachutistes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
